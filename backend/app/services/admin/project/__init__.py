e21814954d273544c79a769d2c02a0b1505d8107
mhsammak <musabbirhasansammak@gmail.com>
2021-03-12 02:14:46 +0600

    sets up postgres container & creates a test route

--- backend/app/services/admin/project/__init__.py
+++ backend/app/services/admin/project/__init__.py
@@ -1,11 +1,25 @@
 import os
 
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+db = SQLAlchemy(app)
+
+class Admin(db.Model):
+    __tablename__ = 'admins'
+    userid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
 
 @app.route('/admin/ping', methods=['GET'])
 def ping():
