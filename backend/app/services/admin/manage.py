4f4de2ec705c0062ba21294f36eb889fa8fb9484
mhsammak <musabbirhasansammak@gmail.com>
2021-03-12 12:50:26 +0600

    updates manage.py and requirements.txt

--- backend/app/services/admin/manage.py
+++ backend/app/services/admin/manage.py
@@ -1,14 +1,26 @@
-from flask.cli import FlaskGroup
+import unittest
+
+from flask_script import Manager
 
 from project import app, db
 
-cli = FlaskGroup(app)
 
-@cli.command()
+manager = Manager(app)
+
+@manager.command
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
 
+@manager.command
+def test():
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
 if __name__ == '__main__':
-    cli()
+    manager.run()
