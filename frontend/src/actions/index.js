5df2b588301641817e35d061faf02e03206db054
mhsammak <musabbirhasansammak@gmail.com>
2021-03-17 02:16:13 +0600

    documents reduct actions and reducers

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,3 +1,8 @@
+// The application currently utilizes 3 redux actions and
+// action creators to persist login information and admin data
+// and GitHub access tokens through a global store. The taks 
+// each action performs should be evident from their names.
+
 export const UPDATE_LOGIN_STATUS = 'UPDATE_LOGIN_STATUS';
 export const UPDATE_ADMIN_DATA = 'UPDATE_ADMIN_DATA';
 export const UPDATE_ADMIN_TOKEN = 'UPDATE_ADMIN_TOKEN';
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -6,17 +6,22 @@ import {
     UPDATE_ADMIN_TOKEN
 } from '../actions';
 
-
+// Initially user is set to logged out mode.
 const initialLoginState = {
     loggedIn: false
 }
 
+// Since user is logged out initially, all admin data are set to
+// null at the begining.
 const initialAdminData = {
     id: null,
     email: null,
     token: null
 }
 
+// The login reducer is responsible for managing login state
+// of a user. It takes only one action type UPDATE_LOGIN_STATUS
+// to set loggedIn variable to either true or false.
 function login(initialState = initialLoginState, action) {
     const {loggedIn} = action;
     switch(action.type) {
@@ -30,6 +35,10 @@ function login(initialState = initialLoginState, action) {
     }
 }
 
+// The admin reducer is responsible for maintaining data related
+// to a logged in admin. The UPDATE_ADMIN_DATA action updates data
+// like admin id, email, etc. While, UPDATE_ADMIN_TOKEN sotres &
+// updates GitHub access tokens for subsequent uses.
 function admin(initialState = initialAdminData, action) {
     switch(action.type) {
         case UPDATE_ADMIN_DATA:
